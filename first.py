4f7ab5ff870314f87e8f475f6e5ebe06de6ee2d9
Mejeb <mejebitadaniel@gmail.com>
2023-10-16 15:11:00 +0300

    first commit

---
+++ first.py
@@ -0,0 +1,10 @@
+fnum = input("What is your first number ")
+snum = input("What is your second number ")
+
+if fnum > snum:
+    print("The first numberis larger!")
+
+elif snum > fnum:
+    print("The second number is larger!")
+else:
+    print("The numbers are the same")

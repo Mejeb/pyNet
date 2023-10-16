eac7bd8679cbae424a24c396cf39d8bdb205ebb0
Mejeb <mejebitadaniel@gmail.com>
2023-10-16 17:46:59 +0300

    functions

---
+++ functions.py
@@ -0,0 +1,27 @@
+# import time 
+
+# choice = int(input("what number would you like to choose?\n"))
+
+
+# def function (choice):
+#     for num in range(1,choice):
+#         if num % 3 == 0 and num % 5 ==0:
+#             print("FizzBuzz!")
+#         elif num % 3 == 0:
+#             print("Fizz")
+#         elif num % 5 == 0:
+#             print("Buzz!")
+#         else:
+#             print(num)
+
+# print("about to run the program")
+# time.sleep(5)
+# function(choice)
+
+
+Uname = input("What is your name?\n")
+
+def greetings(Uname):
+    print(f"Hello {Uname}")
+
+greetings(Uname)
